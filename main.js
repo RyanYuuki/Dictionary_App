d27e2f6724a3e26a9580d3105aeafa3a0d2efe8b
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-05-05 23:40:51 +0530

    Improved Logic with Error Handling

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ const wordElmt = document.getElementsByClassName("Word")[0];
 const PronounciationElmt = document.getElementsByClassName("Pronounciation")[0];
 const MeaningElmt = document.getElementsByClassName("Meaning")[0];
 const ExampleElmt = document.getElementsByClassName("Example")[0];
-const SoundElmt = document.getElementById("Sound");
-let audioUrl; 
+const SoundElmt = document.getElementById("Sound"); 
 async function searchWord() {
+    let counter = 0;
     const Response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchBarElmt.value}`);
     const Data = await Response.json();
-    console.log(Data);
     wordElmt.innerHTML = searchBarElmt.value;
     for(let i = 0; i < Data.length; i++) {
         for(let j = 0; j < Data[i].phonetics.length; j++) { 
@@ -18,20 +17,30 @@ async function searchWord() {
         {
             for(let x = 0; x < Data[i].meanings[j].definitions.length; x++)
             {
-                if (Data[i].meanings[j].definitions[x].definition != '' ) {
-                    MeaningElmt.innerHTML = Data[i].meanings[j].definitions[x].definition;
+                if(counter == 0)
+                {
+                    if (Data[i].meanings[j].definitions[x].definition != '' ) {
+                        MeaningElmt.innerHTML = Data[i].meanings[j].definitions[x].definition;
+                        counter = 1;
+                    }
                 }
-                if (Data[i].meanings[j].definitions[x].example != '') {
-                    ExampleElmt.innerHTML = Data[i].meanings[j].definitions[x].example;
+                if (counter == 1) {
+                    if (Data[i].meanings[j].definitions[x].example != '') {
+                        ExampleElmt.innerHTML = Data[i].meanings[j].definitions[x].example;
+                        counter = 2;
+                    }
                 }
             }
             
         }
     }
-            SoundElmt.src = `${Data[0].phonetics[0].audio}`;
+    SoundElmt.src = `${Data[0].phonetics[0].audio}`;
+    if (ExampleElmt.textContent == "undefined")
+    {
+        ExampleElmt.innerHTML = "No Example Found";
+    }
 }
 
 document.getElementById("audioBtn").addEventListener('click', () => {
-    console.log(document.body.innerHTML);
     SoundElmt.play();
 });
